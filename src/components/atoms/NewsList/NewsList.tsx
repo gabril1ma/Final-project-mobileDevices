d134b166d720e5f3a97cfa3b7b2659917b49d0a9
“Gabriel <gabrielxapra@gmail.com>
2024-06-03 17:29:27 -0300

    feat: return if 0 news is found

--- src/components/atoms/NewsList/NewsList.tsx
+++ src/components/atoms/NewsList/NewsList.tsx
@@ -1,14 +1,17 @@
 import React from "react";
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, View, Text } from "react-native";
 import NewsItem from "../NewsItems/newsItems";
 
 const NewsList = ({ news }) => {
   return (
     <View style={styles.container}>
+      {news.length === 0 && (
+        <Text style={styles.noNewsText}>Não foram encontradas notícias</Text>
+      )}
       {news?.map((item) => (
         <NewsItem
           key={item.title}
-          broadcaster = {item.broadcaster}
+          broadcaster={item.broadcaster}
           title={item.title}
           imageUrl={item.image}
           date={item.date}
@@ -24,6 +27,12 @@ const styles = StyleSheet.create({
   container: {
     padding: 10,
     width: "60%",
+  },
+  noNewsText: {
+    textAlign: "center",
+    color: "#C4001F",
+    fontSize: 16,
+    marginTop: 20,
   },
 });
 
